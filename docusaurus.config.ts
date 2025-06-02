5de2a1f65f584b77a8a125db13226a3855b2985d
Surjit Kumar Sahoo <surjit@Surjits-MacBook-Pro.local>
2025-06-02 08:11:53 +0530

    fix base url for gh pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -25,8 +25,7 @@ const config: Config = {
   url: `https://${organizationName.toLowerCase()}.github.io`,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  // baseUrl: `/${repoName}/`,
-  baseUrl: '/',
+  baseUrl: `/${repoName}/`,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
